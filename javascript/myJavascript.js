304b34697fa4641946a50362c0731e857c939672
BMCarr <birdie.carr@gmail.com>
2022-01-16 20:35:52 -0800

    Added html, js, css files

---
+++ javascript/myJavascript.js
@@ -0,0 +1,8 @@
+var i =0;
+let array = []; //store each word in the array
+
+for (var i = 0; i< 3; i++){ //loop 3 times
+    let wordsInput = prompt("Please enter a word");
+    array[i] = wordsInput;
+}
+
